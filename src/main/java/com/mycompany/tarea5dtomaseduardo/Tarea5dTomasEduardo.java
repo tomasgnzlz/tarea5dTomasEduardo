2ae904e36c87667251509ef61dc0b956fa94bec8
tgonati327 <tgonati327@g.educaand.es>
2024-01-15 09:29:29 +0100

    commit inicial

---
+++ src/main/java/com/mycompany/tarea5dtomaseduardo/Tarea5dTomasEduardo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea5dtomaseduardo;
+
+/**
+ *
+ * @author tomas
+ */
+public class Tarea5dTomasEduardo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
