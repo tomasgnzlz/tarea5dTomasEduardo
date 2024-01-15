17c0eeff27c2e149e2482d4943e79457f6920a1d
ecorara446 <ecorara446@g.educaand.es>
2024-01-15 09:42:30 +0100

    Creación de los atributos encapsulados y de los contructores (ECA)

---
+++ src/main/java/daw/Zapatos.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package daw;
+
+/**
+ *
+ * @author edu
+ */
+public class Zapatos {
+     String color ;
+     double talla;
+}
